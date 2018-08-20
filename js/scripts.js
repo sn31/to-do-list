b50d1235e71809c1e7b624f11dbc1546c52d3d6b
SkyeN <skye@dames.es>
2018-08-20 09:34:10 -0700

    add back end logic to create the Task constructor

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Task(taskDesceiption,status) {
+    this.taskDesceiption = taskDesceiption;
+    this.status = status;
+} 
+
+$(document).ready(function() {
+    $("form#input").submit(function(event) {
+
+    })
+})
