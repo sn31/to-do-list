090aadc0627ecf84d47128345763c5e08604bf92
SkyeN <skye@dames.es>
2018-08-20 09:51:51 -0700

    add back end and front end logic to add a task and show it next to a checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 function Task(taskDesceiption,status) {
-    this.taskDesceiption = taskDesceiption;
+    this.taskDescription = taskDesceiption;
     this.status = status;
 } 
 
 $(document).ready(function() {
     $("form#input").submit(function(event) {
-
+        event.preventDefault();
+        var taskDescriptionInput = $("#taskInput").val();
+        var newTask = new Task(taskDescriptionInput,false);
+        $("#taskInput").val("");
+        $("#result").show();
+        $("#result").append("<input name='statusBox' type='checkbox'/> &nbsp;&nbsp;" + newTask.taskDescription + "<br>")
+        var taskStatus = $("input:checkbox[name=statusBox]").val();
+        console.log(taskStatus);
     })
 })
