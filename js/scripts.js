a88688c040dc85ed7b9c150eec7e82d43fb15991
SkyeN <skye@dames.es>
2018-08-20 11:09:58 -0700

    add unbind the click function for the list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-function Task(taskDescription,status) {
+function Task(taskDescription, status) {
     this.taskDescription = taskDescription;
-} 
-
-Task.prototype.removeTask = function() {
-    this.taskDescription ="TESTING";
 }
-$(document).ready(function() {
-    $("form#input").submit(function(event) {
+
+Task.prototype.removeTask = function () {
+    this.taskDescription = "TESTING";
+}
+$(document).ready(function () {
+    $("form#input").submit(function (event) {
         event.preventDefault();
         var taskDescriptionInput = $("#taskInput").val();
         var taskStatus = $("input[type=checkbox]:checked").val();
         var newTask = new Task(taskDescriptionInput);
         $("#taskInput").val("");
         $("#taskList").show();
-        $("#taskList").append("<li><input type='checkbox' value='test'/> &nbsp;&nbsp;" + newTask.taskDescription + "<br></li>")
-        
-        $("#taskList li").click(function () {
+        $("#taskList").append("<li><input type='checkbox'/> &nbsp;&nbsp;" + newTask.taskDescription + "<br></li>")
+        $("#taskList li").unbind('click').click(function () {
             $(this).remove();
-            $("#completedList").append($(this).text())
+            console.log($(this).text());
+            $("#completedList").append("<li>"+$(this).text()+"<li>");
         })
-        
+
     })
 })
